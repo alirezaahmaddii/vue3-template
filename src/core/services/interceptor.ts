b3fdf52a17531ccc6fd2734d12d46ed6a2adceed
Alireza Ahmadi <alirezaahmadi1308@gmail.com>
2024-12-04 14:21:50 +0330

    Add `axios` to config

---
+++ src/core/services/interceptor.ts
@@ -0,0 +1,37 @@
+import axios from "axios";
+
+const apiService = axios.create({
+  baseURL: "BASE_URL",
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+apiService.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("authToken");
+    if (token) {
+      config.headers["Authorization"] = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+apiService.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    if (error.response) {
+      console.error("Error response:", error.response);
+    } else {
+      console.error("Network error:", error.message);
+    }
+    return Promise.reject(error);
+  }
+);
+
+export default apiService;
